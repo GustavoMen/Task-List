a68999d930314936275b1b418a12aba75bc1b30b
Gustavo Mendonça <contato.gustavomendonca@gmail.com>
2021-12-09 22:57:14 -0300

    lista de tarefas

--- JS/script.js
+++ JS/script.js
@@ -1,30 +1,50 @@
-const localStorageContent = localStorage.getItem('task')
+var listaTarefa = [];
+var novaTarefa = {};
 
-var task = {
-
-};
-if(localStorageContent === null){
-    task = [];
-}else {
-   task = JSON.parse(localStorageContent);
+function recuperaItens() {
+  let listaTarefaAtualizada = localStorage.getItem("listaTarefa");
+  listaTarefaAtualizada = JSON.parse(listaTarefaAtualizada);
+  let lista = document.querySelector("#item-container");
+  for (let i in listaTarefaAtualizada) {
+    lista.innerHTML += `<div class="item">
+                        <input type="checkbox"> ${listaTarefaAtualizada[i].descricao}
+                        </div>`;
+  }
 }
 
-let inputTask = document.getElementById("addTask")
-
-
-
-function adicionar(){
-    
-    let inputTask = document.getElementById("addTask")
-    let taskValue = inputTask.value;
-    var task = [taskValue]
-
-
-    localStorage.setItem('task',  JSON.stringify(task) );
-
-    console.log(localStorageContent)
-    
-    let h1 = document.getElementById("ulTask")
-
+function criaTarefa(descricao) {
+  this.descricao = descricao;
 }
 
+function add() {
+  let tarefa = document.querySelector("#tarefa");
+  if (tarefa.value == '') {
+    alert("Não deixe o campo em branco!");
+  } else {
+    let lista = document.querySelector("#item-container");
+    lista.innerHTML += `<div class="item">
+                        <input type="checkbox"> ${tarefa.value}
+                        </div>`;
+    novaTarefa = new criaTarefa(tarefa.value);
+    listaTarefa.push(novaTarefa);
+    tarefa.value = "";
+    tarefa.focus();
+  }
+}
+
+function deletar() {
+  listaTarefa = [];
+  let listaItem = document.querySelector("#item-container");
+  listaItem.parentNode.removeChild(listaItem);
+  localStorage.clear();
+  let divLista = document.querySelector('#lista');
+  let divNova = document.createElement("div");
+  divNova.innerHTML = "";                   
+  divLista.appendChild(divNova);
+  divNova.setAttribute('id', 'item-container');
+}
+
+function save() {
+  listaTarefa = JSON.stringify(listaTarefa);
+  localStorage.setItem("listaTarefa", listaTarefa);
+}
